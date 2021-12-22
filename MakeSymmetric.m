916f22b9b6d5ab16547b057671972471f02e0c1b
CH Yoon <kakey98@gmail.com>
2021-12-22 11:36:12 +0900

    adding file

---
+++ MakeSymmetric.m
@@ -0,0 +1,16 @@
+function M = MakeSymmetric(X,mode)
+%행렬의 윗삼각형부분(upper triangle)을 복사해 symmetric matrix를 생성
+%인수에 따라서 윗삼각형을 복사할건지 아래삼각형을 복사할건지 선택가능
+%기본값은 윗삼각형, 추가인수로 "Lower"입력시 아래삼각형을 복사
+    arguments
+        X
+        mode = "Upper"
+
+    end
+
+    
+   
+
+
+end
+
---
+++ main.m
@@ -0,0 +1,5 @@
+
+
+
+x=[1 0 0; 0 1 0; 0 0 1]
+MakeSymmetric(x)
